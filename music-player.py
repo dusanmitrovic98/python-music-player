a958163d62283ccffeca42b0df0578c76d4be028
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-02 22:45:56 +0200

    Update music-player.py

--- music-player.py
+++ music-player.py
@@ -4,3 +4,4 @@ import configparser
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QSlider, QPushButton, QFileDialog, QListWidget, QLabel
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import Qt, QUrl, QTime, QTimer
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
