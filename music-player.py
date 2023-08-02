ab3bc622e452c4bdff743192f7966ec59ce4cf58
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-02 22:49:00 +0200

    Update music-player.py

--- music-player.py
+++ music-player.py
@@ -15,3 +15,4 @@ class MusicPlayer(QWidget):
         self.setStyleSheet(
             "background-color: #1c1c1c; color: #f0f0f0; selection-color: #f0f0f0; selection-background-color: #444444;")
 
+        self.mediaPlayer = QMediaPlayer(self)
