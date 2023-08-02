efd6b1b7dd527331f0ea215fafa3a84d42833b27
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-02 22:59:41 +0200

    Update music-player.py

--- music-player.py
+++ music-player.py
@@ -49,3 +49,4 @@ class MusicPlayer(QWidget):
             "QSlider::handle:horizontal:hover {background: #f0f0f0; border: 1px solid #444; border-radius: 8px;}"
             "QSlider::sub-page:horizontal:disabled {background: #666666; border-color: #999;}"
             "QSlider::handle:horizontal:disabled {background: #666666; border: 1px solid #444; border-radius: 8px;}"
+        )
