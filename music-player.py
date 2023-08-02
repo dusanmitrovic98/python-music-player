08f791782f31a5613bea12a6825662284f60c7c1
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-02 23:04:53 +0200

    Update music-player.py

--- music-player.py
+++ music-player.py
@@ -65,3 +65,4 @@ class MusicPlayer(QWidget):
             "QSlider::handle:horizontal:disabled {background: #666666; border: 1px solid #444; border-radius: 8px;}"
         )
         self.positionSlider.sliderPressed.connect(self.pause_music)
+        self.positionSlider.sliderReleased.connect(self.update_position)
