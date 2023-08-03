ae5e11df368c5dd27ad7e3521430e44d680e188d
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-03 11:58:43 +0200

    Update music-player.py

--- music-player.py
+++ music-player.py
@@ -96,3 +96,4 @@ class MusicPlayer(QWidget):
         main_layout.addWidget(self.musicList)
         main_layout.addWidget(self.positionSlider)
         main_layout.addWidget(self.timeLabel)
+        main_layout.addLayout(controls_layout)
