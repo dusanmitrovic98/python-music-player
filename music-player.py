be8b1b81121a1328780fea8243a8a6435887f6f0
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-03 12:31:47 +0200

    Update music-player.py

--- music-player.py
+++ music-player.py
@@ -194,3 +194,4 @@ class MusicPlayer(QWidget):
             current_time = QTime(0, 0).addMSecs(current_position)
 
             self.timeLabel.setText("{}/{}".format(current_time.toString("mm:ss"), duration_time.toString("mm:ss")))
+
